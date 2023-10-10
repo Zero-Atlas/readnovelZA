9829225a4e2748be8a9907e510b16789bd8515e1
Huynh Dac An <andachuynh@gmail.com>
2023-10-10 23:02:41 +0700

    C3-finish novel list style

--- frontend/client/src/components/NovelItem/NovelItem.js
+++ frontend/client/src/components/NovelItem/NovelItem.js
@@ -1,19 +1,20 @@
 import {Link} from 'react-router-dom'
 import classes from "./NovelItem.module.css";
+import cover from '../../static/cover.jpg'
 
 export default function NovelItem(props) {
   return (
     <li className={classes.listItem}>
       <Link to="/novellink" className={classes.image}>
-        <img src="/imagelink" alt="desc" />
+        <img src={cover} alt="desc" />
       </Link>
       <a href="/novellink" className={classes.title}>
         novelname
       </a>
       <div className={classes.chapter}>
         <p>Chapter</p>
-        <Link to="/latestchapter">newest</Link>
-        <Link to="/2ndlatestchapter">2nd new</Link>
+        <Link to="/latestchapter">101</Link>
+        <Link to="/2ndlatestchapter">100</Link>
       </div>
     </li>
   );
