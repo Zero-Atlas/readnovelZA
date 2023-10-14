d625aa175c576f3dc83edafe68e3ef71b345d440
Huynh Dac An <andachuynh@gmail.com>
2023-10-14 18:31:05 +0700

    C6-finish novel detail page visual and build search page

--- frontend/client/src/pages/Novel/Novel.js
+++ frontend/client/src/pages/Novel/Novel.js
@@ -40,9 +40,15 @@ export default function Novel(props) {
               <li>
                 <Link to="/search?genre=action">Action</Link>
               </li>
-              <li><Link to="/search?genre=comedy">Comedy</Link></li>
-              <li><Link to="/search?genre=drama">Drama</Link></li>
-              <li><Link to="/search?genre=super-nature">Super Nature</Link></li>
+              <li>
+                <Link to="/search?genre=comedy">Comedy</Link>
+              </li>
+              <li>
+                <Link to="/search?genre=drama">Drama</Link>
+              </li>
+              <li>
+                <Link to="/search?genre=super-nature">Super Nature</Link>
+              </li>
             </ul>
           </div>
           <div className={classes.action}>
@@ -50,10 +56,64 @@ export default function Novel(props) {
               <button>Follow</button>
               <span>2048 follower</span>
             </div>
-            <Link className={classes.continue} to="/continue||latest">Continue from chapter 99</Link>
-            <Link className={classes.start} to="/start">Read from start</Link>
+            <Link className={classes.continue} to="/continue||latest">
+              Continue from chapter 99
+            </Link>
+            <Link className={classes.start} to="/start">
+              Read from start
+            </Link>
           </div>
         </div>
+      </div>
+      <div className={classes.description}>
+        <h2>Description:</h2>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites of the word in classical literature, discovered the
+          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the
+          theory of ethics, very popular during the Renaissance. The first line
+          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+          section 1.10.32.
+        </p>
+      </div>
+      <div className={classes.chapters}>
+        <h2>Chapter list:</h2>
+        <table className={classes.table}>
+          <thead>
+            <tr>
+              <th className={classes.chapter}>Chapter</th>
+              <th className={classes.title}>Title</th>
+              <th className={classes.group}>Translate Group</th>
+              <th className={classes.status}>Status</th>
+              <th className={classes.viewed}>Viewed</th>
+              <th className={classes.updated}>Updated</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2</td>
+              <td>N/A</td>
+              <td></td>
+              <td>avalable</td>
+              <td>0</td>
+              <td>3 hours ago</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>N/A</td>
+              <td></td>
+              <td>avalable</td>
+              <td>214</td>
+              <td>18 hours ago</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- frontend/client/src/pages/Search/Search.js
+++ frontend/client/src/pages/Search/Search.js
@@ -1,5 +1,90 @@
-import classes from './Search.module.css'
+import { Link } from "react-router-dom";
+import classes from "./Search.module.css";
+import cover from "../../static/cover.jpg";
 
-export default function Search(){
-  return <div className={classes.wp}></div>
+export default function Search() {
+  return (
+    <div className={classes.wp}>
+      <div className={classes.title}>Novel list</div>
+      <div className={classes.grid}>
+        <div className={classes.sort}>
+          <button className={classes.recent}>Recent</button>
+          <button className={classes.new}>New</button>
+          <button className={classes.view}>View</button>
+        </div>
+        <div className={classes.page}>
+          <button className={classes.prev}>Previous</button>
+          <button className={classes.next}>Next</button>
+        </div>
+        {/* ----------------------Search result------------------------ */}
+        <ul className={classes.result}>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/novel/novel-name">
+              <div className={classes.image}>
+                <img src={cover} alt="novel-name" />
+              </div>
+              <p>Novel Name</p>
+            </Link>
+          </li>
+        </ul>
+        <div className={classes.page}>
+          <button className={classes.prev}>Previous</button>
+          <button className={classes.next}>Next</button>
+        </div>
+        {/* -----------------------Genre field------------------------- */}
+        <div className={classes.genre}>
+          <fieldset>
+            <legend>Genre</legend>
+            <ul className={classes.category}>
+              <li>Action</li>
+              <li>Adventure</li>
+              <li>Comedy</li>
+              <li>Drama</li>
+              <li>Romatic</li>
+            </ul>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
 }
